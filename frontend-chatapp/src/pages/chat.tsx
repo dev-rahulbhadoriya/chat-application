7d964f57543c8ad7ad713bde31b8c0b2a7ff8b9b
dev-rahulbhadoriya <rbhadoria97@gmail.com>
2024-03-14 14:31:47 +0530

    Fix: chat.tsx

--- frontend-chatapp/src/pages/chat.tsx
+++ frontend-chatapp/src/pages/chat.tsx
@@ -1,7 +1,5 @@
 import {
   PaperAirplaneIcon,
-  PaperClipIcon,
-  XCircleIcon,
 } from "@heroicons/react/20/solid";
 import { useEffect, useRef, useState } from "react";
 import { getChatMessages, getUserChats, sendMessage } from "../api";
